f712e30088ba19d811325a0f1b823e7899cb1b0f
wesleylaiks <wesleylai.ks@gmail.com>
2024-11-23 17:54:12 -0500

    Initial Commit

---
+++ Swim/SwimApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwimApp.swift
+//  Swim
+//
+//  Created by Wesley Lai on 11/23/24.
+//
+
+import SwiftUI
+
+@main
+struct SwimApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwimTests/SwimTests.swift
@@ -0,0 +1,17 @@
+//
+//  SwimTests.swift
+//  SwimTests
+//
+//  Created by Wesley Lai on 11/23/24.
+//
+
+import Testing
+@testable import Swim
+
+struct SwimTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
